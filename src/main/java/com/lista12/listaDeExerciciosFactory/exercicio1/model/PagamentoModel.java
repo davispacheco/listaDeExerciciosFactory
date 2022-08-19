659b494458641141569ec0b0c6865eafbec32887
davispacheco <davipacheco1996@gmail.com>
2022-08-18 22:49:16 -0300

    Adicionar atributos à classe PagamentoModel.

--- src/main/java/com/lista12/listaDeExerciciosFactory/exercicio1/model/PagamentoModel.java
+++ src/main/java/com/lista12/listaDeExerciciosFactory/exercicio1/model/PagamentoModel.java
@@ -1,4 +1,21 @@
 package com.lista12.listaDeExerciciosFactory.exercicio1.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "pagamento")
 public class PagamentoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    @Column(length = 10, nullable = false)
+    private BigDecimal valorAPagar;
+    @Column(length = 10, nullable = false)
+    private BigDecimal diferencaValor;
+    @Column(length = 10, nullable = false)
+    private BigDecimal valorPago;
 }
